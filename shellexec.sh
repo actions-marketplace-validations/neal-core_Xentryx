8f7e957c5c2a5ca3da25bc405778778699d041af
Harrison John-Anozie <harrisjohnu@gmail.com>
2026-09-14 22:59:31 +0100

    Fix conditional syntax in shellexec.sh

--- shellexec.sh
+++ shellexec.sh
@@ -7,9 +7,9 @@ failures=0
 flutter doctor
 status=$?
 
-if [ $status -ne 0]; then
+if [[ $status -ne 0 ]]; then
     echo "Flutter is not installed or not configured properly. Please install Flutter and set up your environment."
-    failures=$((failures + 1))
+    failures=$(( failures + 1 ))
 else
     echo "Flutter is installed and configured properly."
 fi
@@ -20,9 +20,9 @@ flutter pub get
 
 
 status=$?
-if [ $status -ne 0 ]; then
+if [[ $status -ne 0 ]]; then
     echo "Failed to get dependencies. Please check your pubspec.yaml file and try again."
-    failures=$((failures + 1))
+    failures=$(( failures + 1 ))
 else
     echo "Dependencies fetched successfully."
 fi
@@ -30,9 +30,9 @@ fi
 echo "Analyzing Dart Code..."
 flutter analyze
 status=$?
-if [ $status -ne 0 ]; then
+if [[ $status -ne 0 ]]; then
     echo "Dart code analysis failed. Please fix the issues reported by the analyzer."
-    failures=$((failures + 1))
+    failures=$(( failures + 1 ))
 else
     echo "Dart code analysis passed successfully."
 fi
@@ -40,15 +40,15 @@ fi
 echo "Running Tests..."
 flutter test
 status=$?
-if [ $status -ne 0 ]; then
+if [[ $status -ne 0 ]]; then
     echo "Tests failed. Please fix the failing tests."
-    failures=$((failures + 1))
+    failures=$(( failures + 1 ))
 else
     echo "All tests passed successfully."
 fi
 echo
 echo "==============================="
-if [$failures -eq 0]; then
+if [[ $failures -eq 0 ]]; then
     echo "All checks passed successfully."
     exit 0
 else
